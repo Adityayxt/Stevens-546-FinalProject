d746c96b9c21b64b1a9f110de859cbead9bb089c
QIQU0726 <1207351531@qq.com>
2025-08-12 23:08:52 +0800

    QuQi

---
+++ config/mongoConnection.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('✅ MongoDB connected');
+});
+
+export default mongoose;
